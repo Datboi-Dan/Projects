0c7c233e1c43c4de1059f8dcaaf339cdfd24d12d
Datboi-Dan <datboidan961@gmail.com>
2025-05-20 10:45:29 -1000

    Create StatusEffect.java

---
+++ MakeYourChoiceGame/StatusEffect.java
@@ -0,0 +1,45 @@
+
+
+public enum StatusEffect
+{
+
+	//Common Negative Status Effects.
+	SLEEP,
+	SILENCE,
+	DARKNESS,
+	
+	//Uncommon Negative Status Effects.
+	PETRIFY,
+	CONFUSE,
+	BERSERK,
+	ZOMBIE,
+	DOOM,
+	CURSE,
+	
+	//Affects physical damage.
+	PUMP,
+	WITHER,
+	
+	//Affects crit chance and damage.
+	PRECISION,
+	UNSTABLE,
+	
+	//Affects magic damage.
+	FOCUS,
+	DISTRACT,
+	
+	//Affects physical damage mitigator
+	HARDEN,
+	FRAIL,
+	
+	//Affects magic damage mitigator
+	SHELL,
+	TARGETED,
+	
+	//Positive Status Effects
+	REFLECT,
+	REGEN,
+	AUTOLIFE
+	
+	
+}
